d34a554120a9b4ae63f9c47c0c8d5685b6e7c81b
Ishan Shah <the_ishana@MacBookPro.lan>
2025-06-01 15:14:56 -0500

    Add project structure for Web2b

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 function callAPI() {
-  fetch("https://your-api-url.com/sample")
+  fetch("https://backend-app.azurewebsites.net/api1/sample")
     .then(response => response.json())
     .then(data => alert(JSON.stringify(data)))
     .catch(error => console.error('API Error:', error));
 }
+
